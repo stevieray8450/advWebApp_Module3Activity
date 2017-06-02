f69e4027a551f606a8e0bf4f865a5977a8bd6f93
Stephen Boniface <stevieray8450@gmail.com>
2017-06-01 22:00:39 -0500

    first commit

--- mypage.php
+++ mypage.php
@@ -1,5 +1,3 @@
-<?php
-
 require_once 'vendor\autoload.php';
 
 use WindowsAzure\Common\ServicesBuilder;
@@ -19,5 +17,3 @@ catch(ServiceException $e){
     // Error codes and messages can be found here:
     // http://msdn.microsoft.com/library/azure/dd179438.aspx
 }
-
-?>
